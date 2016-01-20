eea27c24ed5e3cd27d4084fafcfa4205f6706efe
Jun_K <junktym123@gmail.com>
2016-01-19 18:43:20 -0800

    Separte ClockView. Use layer instead of images. Add gitignore

---
+++ ClockView.swift
@@ -0,0 +1,13 @@
+//
+//  ClockView.swift
+//  Tokei
+//
+//  Created by Jumpei Katayama on 2/10/15.
+//  Copyright (c) 2015 Jumpei Katayama. All rights reserved.
+//
+
+import Cocoa
+
+class ClockView: UIView {
+
+}
